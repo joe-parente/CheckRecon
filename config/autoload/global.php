3e1121c3ba696f34da9197321159b2f617554eef
Joe Parente <joe@joeparente.com>
2014-10-07 21:24:04 -0400

    AuthMod works again after I blew away the .git dir and the half my working copy. sheesh!

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Global Configuration Override
  *
@@ -10,7 +11,22 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
 return array(
-    // ...
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=zf2tutorial;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+        'standard_identity' => function ($sm) {
+    $roles = array('guest', 'member', 'admin');
+    $identity = new \ZfcRbac\Identity\StandardIdentity($roles);
+    return $identity;
+}),
 );
